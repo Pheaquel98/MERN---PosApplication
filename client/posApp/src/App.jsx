2db2ec7c5c1cb2f54c5968b51a6e303e9e50f0e1
pheaquel98 <gallactofener123@hotmail.com>
2023-04-27 14:07:15 +0300

    some styling changes

--- client/posApp/src/App.jsx
+++ client/posApp/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Button, Space } from "antd"
-import Header from "./components/Header/Header"
+import Header from "./components/header/Header"
 import Categories from "./components/categories/Categories"
 import Products from "./components/products/Products"
 import CartTotals from "./components/cart/CartTotals"
@@ -11,11 +11,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className="home px-6 flex justify-between gap-10">
-        <div className="categories overflow-auto max-h-[calc(100vh-_-104px)] pb-60">
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+        <div className="categories overflow-auto max-h-[calc(100vh_-_104px)] md:pb-6">
           <Categories />
         </div>
-        <div className="products flex-[8]">
+        <div className="products flex-[8] max-h-[calc(100vh_-_104px)]">
           <Products />
         </div>
         <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
--- client/posApp/src/components/categories/Categories.jsx
+++ client/posApp/src/components/categories/Categories.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Categories = () => {
   return (
-    <ul className="flex flex-col gap-4 text-lg">
+    <ul className="flex md:flex-col gap-4 text-lg">
       <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
         <span>All</span>
       </li>
