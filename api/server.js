ffa5c0bd5086f9723464069abd2cc66db444b569
pheaquel98 <gallactofener123@hotmail.com>
2023-05-02 18:04:39 +0300

    connected with mongodb and models created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-console.log("hello big world")
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const app = express()
+const port = 5000
+
+dotenv.config()
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to MongoDB")
+  } catch (error) {
+    throw error
+  }
+}
+
+app.get("/", (req, res) => res.send("hello"))
+
+app.listen(port, () => {
+  connect()
+  console.log(`Example app listening on port ${port}`)
+})
