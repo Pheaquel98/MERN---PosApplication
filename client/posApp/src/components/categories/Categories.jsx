a83fe61335b85516936f9d98037a1686735f0739
pheaquel98 <gallactofener123@hotmail.com>
2023-05-03 21:40:23 +0300

    category post

--- client/posApp/src/components/categories/Categories.jsx
+++ client/posApp/src/components/categories/Categories.jsx
@@ -1,29 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
+import { PlusOutlined } from "@ant-design/icons"
+import { Button, Form, Input, Modal, message } from "antd"
+import "./style.css"
 
 const Categories = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+  const [form] = Form.useForm()
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+      message.success("Category Added")
+      form.resetFields()
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <ul className="flex md:flex-col gap-4 text-lg">
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Foods</span>
+      <li className="category-item">
+        <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Drinks</span>
+      <li className="category-item">
+        <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Drinks</span>
+      <li className="category-item">
+        <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Drinks</span>
+      <li className="category-item">
+        <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Drinks</span>
+      <li className="category-item">
+        <span>All</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-green-800 transition-all text-center min-w-[145px]">
-        <span>Drinks</span>
+      <li
+        className="category-item !bg-purple-800 hover:opacity-90"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined className="md:text-2xl" />
       </li>
+      <Modal
+        title="Add New Category"
+        open={isAddModalOpen}
+        onCancel={() => setIsAddModalOpen(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            name="title"
+            label="Add Category"
+            rules={[
+              {
+                required: true,
+                message: "Please fill empty fields!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Create
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   )
 }
